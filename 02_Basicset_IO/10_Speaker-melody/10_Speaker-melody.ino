9bcbef856caa7076e53fec0befda9dd936b4454d
fj3776aiiot <fj3776aiiot@gmail.com>
2026-04-09 13:23:00 +0900

    Chage DQ

--- 02_Basicset_IO/10_Speaker-melody/10_Speaker-melody.ino
+++ 02_Basicset_IO/10_Speaker-melody/10_Speaker-melody.ino
@@ -1,112 +1,127 @@
-const int spkrPin = 3; //3:ConnectorA 4:ConnectorB
+const int spkrPin = 3; 
 
-#define BEAT 230
-#define DO 261.6
-#define _DO 277.18
-#define RE 293.665
-#define _RE 311.127
-#define MI 329.63
-#define FA 349.228
-#define _FA 369.994
-#define SO 391.995
-#define _SO 415.305
-#define RA 440
-#define _RA 466.164
-#define TI 493.883
-#define octDO 523.251
+// 音程の定義（1オクターブ上を追加）
+#define DO  261.6
+#define RE  293.7
+#define MI  329.6
+#define FA  349.2
+#define SO  392.0
+#define RA  440.0
+#define TI  493.9
+#define octDO 523.3
+#define HI_RE 587.3
+#define HI_MI 659.3
+#define HI_FA 698.5
+#define HI_FAs 740.0
+#define HI_SO 784.0
+#define HI_RA 880.0
+#define HI_TI 988.0
+#define HI_HI_DO 1047.0
 
-void doremi(){
-  ledcWriteTone(spkrPin, DO);
-  delay(250);
-  ledcWriteTone(spkrPin, RE);
-  delay(250);
-  ledcWriteTone(spkrPin, MI);
-  delay(250);
-  ledcWriteTone(spkrPin, FA);
-  delay(250);
-  ledcWriteTone(spkrPin, SO);
-  delay(250);
-  ledcWriteTone(spkrPin, RA);
-  delay(250);
-  ledcWriteTone(spkrPin, TI);
-  delay(250);
-  ledcWriteTone(spkrPin, octDO);
-  delay(250);
-  ledcWriteTone(spkrPin, 0); // no sound
-  delay(250);
-}
+void playmusic() {
+  // --- ファンファーレ冒頭 ---
+  // ソ・ソ・ソ・ド（ドーレミファーソ！）
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
 
-void melodychime(){
-  ledcWriteTone(spkrPin, RA);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, DO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, octDO);
-  delay(BEAT * 2);  
-  ledcWriteTone(spkrPin, DO);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, RA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, DO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT * 3);
-  ledcWriteTone(spkrPin, 0);
-  delay(BEAT);
-}
+  ledcWriteTone(spkrPin, RE); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
+  
+  ledcWriteTone(spkrPin, DO); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, FA); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
 
-void fryer(){
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, 0);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, 0);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, 0);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, FA);
-  delay(BEAT);  
-  ledcWriteTone(spkrPin, SO);
-  delay(BEAT);
-  ledcWriteTone(spkrPin, 0);
-  delay(BEAT);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, FA); delay(200);
+  ledcWriteTone(spkrPin, SO); delay(200);
+  ledcWriteTone(spkrPin, RA); delay(200);
+  ledcWriteTone(spkrPin, DO); delay(200);
+  ledcWriteTone(spkrPin, RA); delay(200);
+  
+  ledcWriteTone(spkrPin, SO); delay(200);
+  ledcWriteTone(spkrPin, FA); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
+  
+  ledcWriteTone(spkrPin, RE); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(200);
+  
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, DO); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, RE); delay(1200);
+  
+  
+  
+  ledcWriteTone(spkrPin, SO); delay(200);
+  ledcWriteTone(spkrPin, SO); delay(200);
+  ledcWriteTone(spkrPin, octDO); delay(400);
+  ledcWriteTone(spkrPin, HI_RE); delay(400);
+  ledcWriteTone(spkrPin, HI_MI); delay(400);
+  ledcWriteTone(spkrPin, HI_FA); delay(400);
+  ledcWriteTone(spkrPin, HI_SO); delay(400);
+  ledcWriteTone(spkrPin, HI_HI_DO); delay(600);
+  
+  
+  ledcWriteTone(spkrPin, HI_TI); delay(200);
+  ledcWriteTone(spkrPin, HI_RE); delay(200);
+  ledcWriteTone(spkrPin, HI_RE); delay(400);
+  ledcWriteTone(spkrPin, HI_SO); delay(300);
+  
+  ledcWriteTone(spkrPin, HI_RA); delay(600);
+  
+  
+  
+  ledcWriteTone(spkrPin, HI_FAs); delay(600);
+  ledcWriteTone(spkrPin, HI_FAs); delay(600);
+  ledcWriteTone(spkrPin, HI_RA); delay(600);
+  ledcWriteTone(spkrPin, HI_SO); delay(400);
+  ledcWriteTone(spkrPin, HI_MI); delay(400);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(200);
+  ledcWriteTone(spkrPin, MI); delay(400);
+  ledcWriteTone(spkrPin, MI); delay(400);
+  ledcWriteTone(spkrPin, HI_FAs); delay(600);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+  
+
+//
+//  ledcWriteTone(spkrPin, SO); delay(150);
+//  ledcWriteTone(spkrPin, RA); delay(150);
+//  ledcWriteTone(spkrPin, TI); delay(150);
+//  ledcWriteTone(spkrPin, octDO); delay(150);
+//  ledcWriteTone(spkrPin, HI_RE); delay(600);
+//
+//  // --- 続くメロディ ---
+//  ledcWriteTone(spkrPin, HI_RE); delay(150);
+//  ledcWriteTone(spkrPin, HI_MI); delay(150);
+//  ledcWriteTone(spkrPin, HI_FA); delay(150);
+//  ledcWriteTone(spkrPin, HI_SO); delay(600);
+//
+//  // 最後のキメ
+//  ledcWriteTone(spkrPin, HI_SO); delay(200);
+//  ledcWriteTone(spkrPin, HI_FA); delay(200);
+//  ledcWriteTone(spkrPin, HI_MI); delay(200);
+//  ledcWriteTone(spkrPin, HI_RE); delay(200);
+//  ledcWriteTone(spkrPin, octDO); delay(800);
+
+  ledcWriteTone(spkrPin, 0); // 停止
 }
 
 void setup() {
   pinMode(spkrPin, OUTPUT);
-  ledcAttach(spkrPin, 12000, 8); //Pin setting(Pin num, Max frequency, Resolution)
+  ledcAttach(spkrPin, 12000, 8);
 }
 
 void loop() {
-  doremi();
-  melodychime();
-  fryer();
+  playmusic();
+  delay(5000); // 5秒待機してループ
 }
